a024c18572f9cca739eb973a751c18881ee75b18
qijiji <2367279431@qq.com>
2021-05-22 16:07:01 +0800

    第二次提交

--- src/main/java/com/wanghua/controller/HelloController.java
+++ src/main/java/com/wanghua/controller/HelloController.java
@@ -3,5 +3,6 @@ package com.wanghua.controller;
 public class HelloController {
     public static void main(String[] args) {
         System.out.println("111");
+        System.out.println("222");
     }
 }
